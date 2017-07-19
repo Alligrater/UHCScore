9655d5e96199f4c536ef82ff04b39e38941db435
Alligrater <AtXiaoLi@outlook.com>
2017-07-19 16:09:38 +0800

    Enabled Scoreboard

--- src/io/github/Alligrater/UHCScore.java
+++ src/io/github/Alligrater/UHCScore.java
@@ -1,7 +1,16 @@
 package io.github.Alligrater;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class UHCScore extends JavaPlugin{
 	
+	@Override
+	public void onEnable() {
+		Bukkit.getPluginManager().registerEvents(new UHCScoreboard(), this);
+	}
+
+	public void onDisable() {
+		
+	}
 }
--- src/io/github/Alligrater/UHCScoreboard.java
+++ src/io/github/Alligrater/UHCScoreboard.java
@@ -1,7 +1,49 @@
 package io.github.Alligrater;
 
+import java.util.Set;
+
+import org.bukkit.Bukkit;
+import org.bukkit.OfflinePlayer;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.scoreboard.DisplaySlot;
+import org.bukkit.scoreboard.Objective;
+import org.bukkit.scoreboard.Scoreboard;
+import org.bukkit.scoreboard.ScoreboardManager;
 
 public class UHCScoreboard implements Listener{
 	
+	 @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent e) {
+        for (Player all : Bukkit.getOnlinePlayers()) {
+            update(all);
+        }
+    }
+	 
+	 @EventHandler
+	    public void onPlayerLeave(PlayerQuitEvent e) {
+	        for (Player all : Bukkit.getOnlinePlayers()) {
+	        	
+	            update(all);
+        }
+    }
+	
+	
+	public static void update(Player p) {
+		
+		
+		
+		ScoreboardManager manager = Bukkit.getScoreboardManager();
+		Scoreboard scoreboard = manager.getNewScoreboard();
+		
+		Objective obj = scoreboard.registerNewObjective("test", "xp");
+		obj.setDisplaySlot(DisplaySlot.SIDEBAR);
+		
+		obj.setDisplayName("¡ìc- UHC SCORE -");
+		
+		p.setScoreboard(scoreboard);
+	}
 }
